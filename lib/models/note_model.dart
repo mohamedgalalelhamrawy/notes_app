497bfa0b3309ddc0bbe0aa3a772e30d72ed74ac7
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-02-03 02:00:49 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class notemodel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+  notemodel(
+      {required this.color,
+      required this.date,
+      required this.subtitle,
+      required this.title});
+}
