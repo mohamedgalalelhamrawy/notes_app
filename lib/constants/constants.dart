8ea273d3063e731fd42a1a6d0393e0cb7a1fda4c
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-02-09 23:02:57 +0200

    create type adaptor for note model , init hive and oben box

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,3 +2,4 @@
 import 'package:flutter/animation.dart';
 
 const kprimarycolor =  Color(0xff53EBD6);
+const kNotesBox = 'notes_box';
--- lib/views/notes_app.dart
+++ lib/views/notes_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants/constants.dart';
 
 import '../widgets/addmodalbottomsheet.dart';
 import '../widgets/notes_view_body.dart';
@@ -10,13 +11,14 @@ class notesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: kprimarycolor,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         onPressed: () {
         showModalBottomSheet(context: context, builder: ((context) {
           return const AddmodalBottomSheet();
         }));
       },
-      child:const  Icon(Icons.add),
+      child:const  Icon(Icons.add,color: Colors.black,),
       ),
       body: const notesViewBody(),
     );
